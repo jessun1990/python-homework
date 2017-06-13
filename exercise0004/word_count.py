b5e4cbb178a9460a0914c494e3f9f9b818d77b06
jessun <jessun1990@outlook.com>
2017-06-13 11:20:41 +0800

    update exercise0004

--- exercise0004/word_count.py
+++ exercise0004/word_count.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # **第 0004 题：**任一个英文的纯文本文件，统计其中的单词出现的个数。
 import re
 import codecs
@@ -7,7 +8,7 @@ def count_words(file):
     with codecs.open(file, 'r', encoding="utf-8") as f:
         file_text = f.read()
         word = re.findall(r'[a-zA-Z0-9]+', file_text)
-    print(len(word))
+    print(str(file) + "单词的个数为" + str(len(word)))
     
 if __name__ == '__main__':
     count_words("Article.txt")
